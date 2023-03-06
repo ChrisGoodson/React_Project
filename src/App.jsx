0080fc4d2f9633ad1c717ef08a0b37983aea49e2
Chris <f.chrisgoodson@icloud.com>
2023-03-06 15:55:03 -0600

    removed greetings

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { Greetings } from "./Greetings";
 
 
 export function App() {
-    return (
-        <>
-        <input type="text" />
-        <input type="text" />
-        <Greetings />
-
-        
-        
-        
-        
-        
-        </>
-
-
-
-    );
+  return (
+    <div></div>
+  );
 }
--- src/Greetings.jsx
+++
@@ -1,9 +0,0 @@
-export function Greetings() {
-  return(
-   <ul>
-        <li>Hello</li>
-        <li>Hi</li>
-        <li>Hey</li>
-  </ul>
-  );
-}
